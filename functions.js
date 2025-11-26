51fd7e237771272f34fcb52b5898b8a5e6a77c9f
Explotador72_Latitude <explotador07@gmail.com>
2025-11-26 11:36:47 +0100

    0.02

--- functions.js
+++ functions.js
@@ -2,6 +2,8 @@
 const imgInput = document.getElementById("imgInput");
 const btnPdf = document.getElementById("convertPdfBtn");
 const previewPdf = document.getElementById("previewPdf");
+const fileName = document.getElementById("nameInput");
+
 
 let images = [];
 
@@ -21,30 +23,55 @@ imgInput.addEventListener("change", () => {
     div.appendChild(img);
     previewPdf.appendChild(div);
   });
-  log.console(images.length === 0);
   btnPdf.disabled = images.length === 0;
 });
 
 // Convertir a PDF
 btnPdf.addEventListener("click", async () => {
+
   if (images.length === 0) return;
-
   btnPdf.textContent = "Generando...";
-
   const { jsPDF } = window.jspdf;
-  const pdf = new jsPDF();
+  let pdf = null;
 
   for (let i = 0; i < images.length; i++) {
+
     const data = await fileToDataURL(images[i]);
+    const img = await loadImage(data);
 
-    if (i > 0) pdf.addPage();  // nueva página para las imágenes siguientes
+    const imgW = img.width;
+    const imgH = img.height;
+    const pxToMm = 0.264583; 
+    const w = imgW * pxToMm;
+    const h = imgH * pxToMm;
 
-    pdf.addImage(data, "JPEG", 10, 10, 190, 0); // ancho auto-escalado
+    // primera página → crear PDF del tamaño exacto
+    if (i === 0) {
+      pdf = new jsPDF({
+        orientation: w > h ? "landscape" : "portrait",
+        unit: "mm",
+        format: [w, h], // <= página del tamaño de la imagen
+      });
+    } else {
+      pdf.addPage([w, h], w > h ? "landscape" : "portrait");
+    }
+
+    // imagen ocupa exactamente toda la hoja
+    pdf.addImage(data, "JPEG", 0, 0, w, h);
   }
-
-  pdf.save("imagenes.pdf");
+  if (!fileName) {fileName="pdfconvert"; console.log("donee")}
+  pdf.save(`${fileName}.pdf`);
   btnPdf.textContent = "Convertir a PDF";
 });
+
+
+function loadImage(src) {
+  return new Promise(resolve => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.src = src;
+  });
+}
 
 // Convierte un file → base64
 function fileToDataURL(file) {
