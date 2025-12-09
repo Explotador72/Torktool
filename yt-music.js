47019eb99cae3b59a704cd2e91a0234146fb938d
Explotador72 <explotador07@gmail.com>
2025-12-09 22:07:04 +0100

    Spotifier

--- yt-music.js
+++ yt-music.js
@@ -5,7 +5,7 @@ const videoName = document.getElementById('nameSong')
 const formatMP4 = document.getElementById('MP4Checkbox')
 const formatMP3 = document.getElementById('MP3Checkbox')
 const previewYT = document.getElementById("previewYT");
-const apiUrlInput = "http://localhost:5000" //"https://yt-dwn-f1c0.onrender.com"
+const apiUrlInput = "https://yt-dwn-f1c0.onrender.com" //"http://localhost:5000" 
 const addUrlBtn = document.getElementById("addUrlBtn");
 
 let ytList = [];
